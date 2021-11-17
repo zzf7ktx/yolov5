55fb179490457d16ea194d32c1309072118fb6c5
zzf7ktx <41227962+zzf7ktx@users.noreply.github.com>
2021-11-17 20:53:36 +0700

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,17 +1,3 @@
-# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
-"""
-Run inference on images, videos, directories, streams, etc.
-
-Usage:
-    $ python path/to/detect.py --weights yolov5s.pt --source 0  # webcam
-                                                             img.jpg  # image
-                                                             vid.mp4  # video
-                                                             path/  # directory
-                                                             path/*.jpg  # glob
-                                                             'https://youtu.be/Zgi9g1ksQHc'  # YouTube
-                                                             'rtsp://example.com/media.mp4'  # RTSP, RTMP, HTTP stream
-"""
-
 import argparse
 import os
 import sys
